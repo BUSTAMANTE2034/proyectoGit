bef0822daab6539524c0e32100c5c7af028d1c64
BUSTAMANTE2034 <bustamanteservincarloseduardo@gmail.com>
2024-09-17 17:01:19 -0600

    Subida 1

---
+++ file1.py
@@ -0,0 +1,13 @@
+#SUMA MULTIPLICACIÓN
+
+a = int (input("Ingresa a: "))
+b = int (input("Ingresa b: "))
+z = input("¿Quieres hacer Suma o Multiplicación S/M?")
+
+match z:
+    #Completar los casos para suma y multiplicación
+    case 'S':
+        print()
+    
+    case _:
+        print("Nada")
